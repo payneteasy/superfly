f3bef6cb3f4755f4091e9735501377d6ba2c2e36
roman <roman@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2009-12-14 16:33:34 +0000

    CSRV-31 Создать каркас приложения superfly (work in progress)
    git-svn-id: https://superfly.googlecode.com/svn/trunk/superfly@167 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

---
+++ superfly-service/src/main/java/com/payneteasy/superfly/service/SSOServiceImpl.java
@@ -0,0 +1,21 @@
+package com.payneteasy.superfly.service;
+
+import com.payneteasy.superfly.api.SSOService;
+import com.payneteasy.superfly.api.SSOUser;
+
+/**
+ * Implementation of SSOService.
+ * 
+ * @author Roman Puchkovskiy
+ */
+public class SSOServiceImpl implements SSOService {
+
+	/**
+	 * @see SSOService#authenticate(String, String)
+	 */
+	public SSOUser authenticate(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
