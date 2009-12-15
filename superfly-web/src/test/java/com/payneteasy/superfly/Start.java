42e54847ab27d9c36ca5e600d33efc0d334a7a85
pavel <pavel@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2009-12-15 11:49:30 +0000

    RESOLVED - issue CSRV-32:Приделать класс Start для тестирования в jetty
    git-svn-id: https://superfly.googlecode.com/svn/trunk/superfly@170 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

---
+++ superfly-web/src/test/java/com/payneteasy/superfly/Start.java
@@ -0,0 +1,55 @@
+package com.payneteasy.superfly;
+
+import javax.naming.NamingException;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Start {
+	 private static final Logger LOG = LoggerFactory.getLogger(Start.class);
+
+	    /**
+	     * -Dwicket.configuration=development or deployment
+	     * -Djetty.port=8085
+	     */
+	    public static void main(String[] args) throws Exception {
+	        new Start().startServer();
+	    }
+	    
+	    public void startServer() throws NamingException {
+	        LOG.info("Configuring jetty web server ...");
+
+	        final Server server = new Server();
+	        SocketConnector connector = new SocketConnector();
+	        // Set some timeout options to make debugging easier.
+	        connector.setMaxIdleTime(1000 * 60 * 60);
+	        connector.setSoLingerTime(-1);
+	        connector.setPort(Integer.parseInt(System.getProperty("jetty.port", "8085")));
+	        server.setConnectors(new Connector[]{connector});
+
+	        WebAppContext paynet = new WebAppContext();
+	        paynet.setServer(server);
+	        paynet.setContextPath("/alerting");
+	        paynet.setWar("src/main/webapp");
+	        
+	        server.addHandler(paynet);
+
+	        try {
+	            System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
+	            server.start();
+
+	            while (System.in.available() == 0) {
+	                Thread.sleep(5000);
+	            }
+	            server.stop();
+	            server.join();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            System.exit(100);
+	        }
+	    }
+}
