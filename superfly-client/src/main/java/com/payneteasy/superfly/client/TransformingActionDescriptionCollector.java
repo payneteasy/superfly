ea28848dc47938452d61484976ce46fd85c60cb7
roman.puchkovskiy <roman.puchkovskiy@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-04-05 07:13:23 +0000

    Correctly transforming scanned action names
    git-svn-id: https://superfly.googlecode.com/svn/trunk/superfly@416 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

---
+++ superfly-client/src/main/java/com/payneteasy/superfly/client/TransformingActionDescriptionCollector.java
@@ -0,0 +1,41 @@
+package com.payneteasy.superfly.client;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Required;
+
+import com.payneteasy.superfly.api.ActionDescription;
+import com.payneteasy.superfly.client.exception.CollectionException;
+
+public class TransformingActionDescriptionCollector implements
+		ActionDescriptionCollector {
+	
+	private ActionDescriptionCollector collector;
+	private StringTransformer[] transformers;
+	
+	@Required
+	public void setCollector(ActionDescriptionCollector collector) {
+		this.collector = collector;
+	}
+
+	@Required
+	public void setTransformers(StringTransformer[] transformers) {
+		this.transformers = transformers;
+	}
+
+	public List<ActionDescription> collect() throws CollectionException {
+		List<ActionDescription> list = collector.collect();
+		for (ActionDescription actionDescription : list) {
+			actionDescription.setName(applyTransformers(actionDescription.getName()));
+		}
+		return list;
+	}
+
+	protected String applyTransformers(String name) {
+		for (StringTransformer transformer : transformers) {
+			name = transformer.transform(name);
+		}
+		return name;
+	}
+
+}
