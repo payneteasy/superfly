18c4546d8bc54b47eae80a110d97b7f79071ec58
pasha.zarubin <pasha.zarubin@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-04-13 10:05:00 +0000

    fix a bug in trunk
    git-svn-id: https://superfly.googlecode.com/svn/trunk/superfly@428 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

---
+++ superfly-remote-api/src/main/java/com/payneteasy/superfly/api/UserExistsException.java
@@ -0,0 +1,14 @@
+package com.payneteasy.superfly.api;
+
+public class UserExistsException extends Exception{
+	public UserExistsException(){}
+	public UserExistsException(String message){
+		super(message);
+	}
+    public UserExistsException(Throwable cause){
+    	super(cause);
+    }
+    public UserExistsException(String message, Throwable cause){
+    	super(message,cause);
+    }
+}
---
+++ superfly-service/src/main/java/com/payneteasy/superfly/model/RegisterUser.java
@@ -0,0 +1,69 @@
+package com.payneteasy.superfly.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+
+public class RegisterUser implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+    private long userid;
+	private String username;
+	private String password;
+	private String email;
+	private long subsystemId;
+	private String principalName;
+	
+	@Column(name = "user_id")
+	public long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+
+	@Column(name = "user_name")
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	@Column(name = "user_password")
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Column(name = "user_email")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Column(name = "ssys_id")
+	public long getSubsystemId() {
+		return subsystemId;
+	}
+
+	public void setSubsystemId(long subsystemId) {
+		this.subsystemId = subsystemId;
+	}
+	@Column(name = "principal_list")
+	public String getPrincipalName() {
+		return principalName;
+	}
+
+	public void setPrincipalName(String principalName) {
+		this.principalName = principalName;
+	}
+
+}
