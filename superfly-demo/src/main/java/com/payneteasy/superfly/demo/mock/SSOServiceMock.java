d8d3c4060a3cb07c81ecebd9e8c823c3fb414821
roman.puchkovskiy <roman.puchkovskiy@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-05-26 05:57:05 +0000

    Updated mock service
    git-svn-id: https://superfly.googlecode.com/svn/trunk/superfly@451 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

--- superfly-demo/src/main/java/com/payneteasy/superfly/demo/mock/SSOServiceMock.java
+++ superfly-demo/src/main/java/com/payneteasy/superfly/demo/mock/SSOServiceMock.java
@@ -14,6 +14,7 @@ import com.payneteasy.superfly.api.SSORole;
 import com.payneteasy.superfly.api.SSOService;
 import com.payneteasy.superfly.api.SSOUser;
 import com.payneteasy.superfly.api.SSOUserWithActions;
+import com.payneteasy.superfly.api.UserExistsException;
 
 /**
  * Mock SSOService implementation.
@@ -66,6 +67,10 @@ public class SSOServiceMock implements SSOService {
 				actions.toArray(new SSOAction[actions.size()])));
 		
 		return users;
+	}
+
+	public void registerUser(String username, String password, String email,
+			long subsystemId, String principalName) throws UserExistsException {
 	}
 
 }
