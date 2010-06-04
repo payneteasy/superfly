58035449ebb7c3c939a96d5cfdadcd9e3c02c5ab
roman.puchkovskiy <roman.puchkovskiy@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-06-04 10:28:49 +0000

    Fixed logging of xml action collector
    git-svn-id: https://superfly.googlecode.com/svn/trunk/superfly@463 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

--- superfly-client/src/main/java/com/payneteasy/superfly/client/ActionDescriptionBean.java
+++ superfly-client/src/main/java/com/payneteasy/superfly/client/ActionDescriptionBean.java
@@ -1,22 +1,7 @@
 package com.payneteasy.superfly.client;
 
-public class ActionDescriptionBean {
-	private String name;
-	private String description;
+import com.payneteasy.superfly.api.ActionDescription;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
+public class ActionDescriptionBean extends ActionDescription {
+	private static final long serialVersionUID = -513189437457390542L;
 }
