3dd40b95f88d329e893ce0f88b8d8534260b2d83
kuccyp <kuccyp@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-10-07 11:38:34 +0000

    user passwords history
    git-svn-id: https://superfly.googlecode.com/svn/branches/superfly-1.2@527 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

--- superfly-sql/mi/R1.2.0/R1.2.0_SSO.sql
+++ superfly-sql/mi/R1.2.0/R1.2.0_SSO.sql
@@ -4,3 +4,19 @@ call run_install_command('alter table users add column secret_question varchar(2
 call run_install_command('alter table users add column secret_answer varchar(255) not null', '42S21');
 call run_install_command('alter table users modify column user_password varchar(128) not null', '42S21');
 call run_install_command('alter table users add column salt varchar(64) not null', '42S21');
+
+
+drop table if exists user_history;
+create table user_history(
+  `user_user_id`         int(10) not null,
+  `user_password` varchar(128) not null,
+  `number_history` int(10) unsigned NOT NULL,                                                                           
+  `start_date` datetime DEFAULT NULL,                                                                                   
+  `end_date` datetime DEFAULT NULL,                                                                                     
+  `update_date` datetime DEFAULT NULL,                                                                                  
+  PRIMARY KEY (`user_user_id`,`number_history`),                                                                        
+  KEY `idx_user_history_start_end_dates` (`user_user_id`,`start_date`,`end_date`),
+  CONSTRAINT `fk_user_history_users` FOREIGN KEY (`user_user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;                                                                                    
+
+
