a0c1b2568966e27f0700b020ae2c8a996c3ab501
kuccyp <kuccyp@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-10-07 10:30:43 +0000

    adding and change column for encrypted password
    git-svn-id: https://superfly.googlecode.com/svn/branches/superfly-1.2@525 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

--- superfly-sql/mi/R1.2.0/R1.2.0_SSO.sql
+++ superfly-sql/mi/R1.2.0/R1.2.0_SSO.sql
@@ -2,3 +2,5 @@ call run_install_command('alter table users add column name varchar(32) not null
 call run_install_command('alter table users add column surname varchar(32) not null', '42S21');
 call run_install_command('alter table users add column secret_question varchar(255) not null', '42S21');
 call run_install_command('alter table users add column secret_answer varchar(255) not null', '42S21');
+call run_install_command('alter table users modify column user_password varchar(128) not null', '42S21');
+call run_install_command('alter table users add column salt varchar(64) not null', '42S21');
