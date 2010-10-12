b8566d9b01c978b397a35ee4fea4598f5cfc1261
roman.puchkovskiy <roman.puchkovskiy@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-10-12 07:55:26 +0000

    Added a console password encryptor
    git-svn-id: https://superfly.googlecode.com/svn/branches/superfly-1.2@583 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

---
+++ superfly-service/src/main/java/com/payneteasy/superfly/password/PasswordEncryptor.java
@@ -0,0 +1,84 @@
+package com.payneteasy.superfly.password;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Encrypts passwords in the database. It assumes they are not encoded yet.
+ * Run without parameters to get help.
+ * WARNING! If some of your passwords are already encrypted, this may
+ * re-encrypt them (i.e. make then non-usable). Encryptor tries to avoid this
+ * by skipping users for which password length matches the new encrypted
+ * password length, but it's just a heuristic.
+ *
+ * @author Roman Puchkovskiy
+ */
+public class PasswordEncryptor {
+	public static void main(String[] args) throws Exception {
+		if (args.length != 4) {
+			printUsage();
+			System.exit(1);
+		}
+		
+		String algorithm = args[0];
+		String url = args[1];
+		String username = args[2];
+		String password = args[3];
+		
+		PasswordEncoder passwordEncoder = createPasswordEncoder(algorithm);
+		SaltGenerator saltGenerator = createSaltGenerator();
+		
+		String test = passwordEncoder.encode("password", saltGenerator.generate());
+		
+		Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+        Connection conn = DriverManager.getConnection (url, username, password);
+        Statement st = conn.createStatement();
+        PreparedStatement prepSt = conn.prepareStatement("update users set user_password = ?, salt = ? where user_id = ?");
+        
+        System.out.println("Starting password encryption");
+        ResultSet rs = st.executeQuery("select user_id, user_name, user_password from users where (user_password is null or user_password = '' or length(user_password) <> " + test.length() + ")");
+        while (rs.next()) {
+        	long id = rs.getLong("user_id");
+        	username = rs.getString("user_name");
+        	password = rs.getString("user_password");
+        	String salt = saltGenerator.generate();
+        	String newPassword = passwordEncoder.encode(password != null ? password : "", salt);
+        	System.out.println(String.format("User %s (%d): old password '%s', salt '%s', new password '%s'",
+        			username, id, password, salt, newPassword));
+        	prepSt.setString(1, newPassword);
+        	prepSt.setString(2, salt);
+        	prepSt.setLong(3, id);
+        	prepSt.addBatch();
+        }
+        rs.close();
+        st.close();
+        prepSt.executeBatch();
+        prepSt.close();
+        
+        conn.close();
+        
+        System.out.println("Finished password encryption");
+	}
+
+	private static void printUsage() {
+		System.out.println("Parameters: <algorithm> <db_url> <db_user> <db_password>");
+		System.out.println("  <algorithm>\t\thash algorithm (for instance, md-5, sha1, sha-256)");
+		System.out.println("  <db_url>\t\tURL of the database (for instance, jdbc:mysql://localhost/sso?characterEncoding=utf8");
+		System.out.println("  <db_user>\t\tusername to connect to database");
+		System.out.println("  <db_password>\t\tpassword of the database user");
+		System.out.println("Example: sha-256 jdbc:mysql://localhost/sso?characterEncoding=utf8 sso 123sso123");
+	}
+
+	private static SHA256RandomGUIDSaltGenerator createSaltGenerator() {
+		return new SHA256RandomGUIDSaltGenerator();
+	}
+
+	private static MessageDigestPasswordEncoder createPasswordEncoder(String algorithm) {
+		MessageDigestPasswordEncoder encoder = new MessageDigestPasswordEncoder();
+		encoder.setAlgorithm(algorithm);
+		return encoder;
+	}
+}
--- superfly-service/src/main/java/com/payneteasy/superfly/password/SaltSource.java
+++ superfly-service/src/main/java/com/payneteasy/superfly/password/SaltSource.java
@@ -2,6 +2,9 @@ package com.payneteasy.superfly.password;
 
 /**
  * Supplies salt values. That salt is used by {@link PasswordEncoder} instances.
+ * This is intended to be stable (i.e. it should return the same instance for
+ * the same argument), unlike {@link SaltGenerator} which generates new value
+ * on each call.
  * 
  * @author Roman Puchkovskiy
  * @see PasswordEncoder
