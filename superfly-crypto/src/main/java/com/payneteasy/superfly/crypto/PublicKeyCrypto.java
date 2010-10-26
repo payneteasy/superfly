b76f6cd8ea2526b7146ba3f0518a7491b772d7c4
roman.puchkovskiy <roman.puchkovskiy@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-10-26 10:35:58 +0000

    Added crypto support to encrypt with public keys infrastructure
    git-svn-id: https://superfly.googlecode.com/svn/branches/superfly-1.3@641 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

---
+++ superfly-crypto/src/main/java/com/payneteasy/superfly/crypto/PublicKeyCrypto.java
@@ -0,0 +1,9 @@
+package com.payneteasy.superfly.crypto;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public interface PublicKeyCrypto {
+	boolean isPublicKeyValid(String armoredPublicKey);
+	void encrypt(byte[] clearText, String name, String armoredPublicKey, OutputStream os) throws IOException;
+}
---
+++ superfly-crypto/src/main/java/com/payneteasy/superfly/crypto/pgp/PGPCrypto.java
@@ -0,0 +1,18 @@
+package com.payneteasy.superfly.crypto.pgp;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.payneteasy.superfly.crypto.PublicKeyCrypto;
+
+public class PGPCrypto implements PublicKeyCrypto {
+
+	public boolean isPublicKeyValid(String armoredPublicKey) {
+		return PGPUtils.isPublicKeyValid(armoredPublicKey);
+	}
+
+	public void encrypt(byte[] clearText, String name, String armoredPublicKey, OutputStream os) throws IOException {
+		PGPUtils.encryptBytesAndArmor(clearText, name, armoredPublicKey, os);
+	}
+
+}
