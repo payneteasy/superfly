06efff67207bcd60d3055f8a35b0181be1e3d636
pasha.zarubin <pasha.zarubin@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2010-11-19 10:31:25 +0000

    admin + пароль с солью
    git-svn-id: https://superfly.googlecode.com/svn/trunk/superfly@714 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

--- superfly-sql/mi/R1.0.0/R1.0.0_SSO_DML.sql
+++ superfly-sql/mi/R1.0.0/R1.0.0_SSO_DML.sql
@@ -26,7 +26,7 @@ insert into role_actions
       ) values
     ((select role_id from roles where role_name = 'admin' and ssys_ssys_id = (select ssys_id from subsystems where subsystem_name = 'superfly')), (select actn_id from actions where action_name = 'admin' and ssys_ssys_id = (select ssys_id from subsystems where subsystem_name = 'superfly')))
 ;
-
+/*
 insert into users
       (
          comp_comp_id, user_name, user_password, is_account_locked, logins_failed, last_login_date
@@ -48,5 +48,5 @@ insert into user_role_actions
       ((select user_id from users where user_name = 'admin'),
             (select ract_id from role_actions where role_role_id = (select role_id from roles where role_name = 'admin' and ssys_ssys_id = (select ssys_id from subsystems where subsystem_name = 'superfly')) and actn_actn_id = (select actn_id from actions where action_name = 'admin' and ssys_ssys_id = (select ssys_id from subsystems where subsystem_name = 'superfly'))))
 ;
-
+*/
 commit;
