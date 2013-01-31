92b974a92905ba2cd810c3ca76e7741873649be4
roman.puchkovskiy <roman.puchkovskiy@11a29e22-99e0-9bb7-d19e-9a0993c63ea3>
2013-01-31 13:21:18 +0000

    - fixed a bug in a procedure
    git-svn-id: https://superfly.googlecode.com/svn/branches/superfly-redirects@869 11a29e22-99e0-9bb7-d19e-9a0993c63ea3

--- superfly-sql/src/get/exchange_subsystem_token.sql
+++ superfly-sql/src/get/exchange_subsystem_token.sql
@@ -3,13 +3,15 @@ delimiter $$
 create procedure exchange_subsystem_token(i_subsystem_token varchar(64))
  main_sql:
   begin
-    declare v_user_id   int(10);
+    declare v_user_id           int(10);
+    declare v_subsystem_name    varchar(32);
 
-    select u.user_id
-      into v_user_id
+    select u.user_id, s.subsystem_name
+      into v_user_id, v_subsystem_name
       from subsystem_tokens st
         join sso_sessions ss on st.ssos_ssos_id = ss.ssos_id
         join users u on ss.user_user_id = u.user_id
+        join subsystems s on st.ssys_ssys_id = s.ssys_id
      where     st.token = i_subsystem_token;
 
     if v_user_id is null then
@@ -26,9 +28,9 @@ create procedure exchange_subsystem_token(i_subsystem_token varchar(64))
 
     call int_get_user_actions(
         v_user_id,
-        i_subsystem_name,
-        i_ip_address,
-        i_session_info,
+        v_subsystem_name,
+        null,
+        null,
         'Y'
     );
   
