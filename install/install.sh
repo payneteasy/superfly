28d5f44ecdf67da878f469e417ae5b621e6ea2a5
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2015-11-05 15:47:53 +0400

    Latest owasp vulnerabilities report: fixes
    - replaced library with a patched version
    - suppressed false positives

---
+++ install/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn install:install-file -Dfile=not-yet-commons-ssl-0.3.17-20151105.jar -DgroupId=ca.juliusdavies -DartifactId=not-yet-commons-ssl -Dpackaging=jar -Dversion=0.3.17-20151105
